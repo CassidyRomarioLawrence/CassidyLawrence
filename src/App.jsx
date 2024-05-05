357f2f52297863fa9d8628006f26c59c4c85d92e
CassidyRomarioLawrence <cassidyromario@gmail.com>
2024-05-05 13:06:39 +0200

    portfolio

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import NavBar from './Navbar'
+import Landing from './Landing'
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Landing />
+    </>
+  )
+}
+
+export default App
---
+++ src/Landing.jsx
@@ -0,0 +1,15 @@
+function Landing(){
+
+    return (
+      <>
+        <div className="intro">
+          <h2 className="geeting">Welcome</h2>
+          <h1 className="name">
+            I'm <span>Cassidy Romario Lawrence</span>
+          </h1>
+        </div>
+      </>
+    );
+}
+
+export default Landing
